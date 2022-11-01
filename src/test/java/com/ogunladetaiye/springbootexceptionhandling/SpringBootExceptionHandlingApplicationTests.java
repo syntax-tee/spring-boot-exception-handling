d5e2044ddf8e22bfd6a8ec2b3a9064bd69f09951
Taiye Emmanuel Ogunlade <ogunladetaiye@gmail.com>
2022-11-01 10:09:22 +0100

    springboot exception handling demonstration

---
+++ src/test/java/com/ogunladetaiye/springbootexceptionhandling/SpringBootExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogunladetaiye.springbootexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
